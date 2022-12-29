310261b8e5869992a7f2dc9257a970d163fae1c3
Roger Mukai <roger@mukaiguy.com>
2022-12-29 10:32:01 -0500

    Added Details View

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -13,5 +13,11 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes',
-                  'price', 'description', 'link']
+                  'price', 'link']
         read_only_fields = ['id']
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """ Serializer for recipe details """"
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -12,7 +12,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """ View for the Recipe APIs"""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -20,3 +20,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """ Retrieves the recipe for the given user """
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """ Returns the serializer class """
+        if self.action =='list':
+            return serializers.RecipeDetailSerializer
+#        elif self.action == 'upload_image':
+#            return serializers.RecipeImageSerializer
+
+        return self.serializer_class
